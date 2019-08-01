ad8394faa8ae986ec22ac05022af722a9f3d6b14
Caoimhan Bradley <caoimhan@alchemytechs.com>
2019-08-01 11:00:49 +0100

    Add project files.

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int index = 1;
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            }while (index <= 5) ;
+
+
+            Console.ReadLine();
+        }
+    }
+}
